8f78e07dd035502d4133d1ff15b54d36aad3b752
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2022-01-09 01:02:38 +0100

    Change coding convention

--- app/src/main/java/com/joaobapt/todo/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/joaobapt/todo/tasklist/TaskListAdapter.kt
@@ -6,30 +6,24 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.joaobapt.todo.R
-import org.w3c.dom.Text
 
-class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>()
-{
+class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
     var currentList: List<Task> = listOf()
     
-    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    {
-        fun bind(taskTitle: String, taskDescription: String)
-        {
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: String, taskDescription: String) {
             itemView.findViewById<TextView>(R.id.task_title).text = taskTitle
             itemView.findViewById<TextView>(R.id.task_description).text = taskDescription
         }
     }
     
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder
-    {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_task, parent, false)
         return TaskViewHolder(itemView)
     }
     
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int)
-    {
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         currentList[position].apply { holder.bind(title, description) }
     }
     
