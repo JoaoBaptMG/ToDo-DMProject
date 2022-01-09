73fb6ac6bf285dec2ea3fc2fbbf3310672bc7cdc
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2022-01-09 03:34:37 +0100

    Adapt MainActivity to view binding

--- app/src/main/java/com/joaobapt/todo/MainActivity.kt
+++ app/src/main/java/com/joaobapt/todo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.joaobapt.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joaobapt.todo.databinding.ActivityFormBinding
+import com.joaobapt.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
