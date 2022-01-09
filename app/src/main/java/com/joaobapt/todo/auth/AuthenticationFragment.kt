80218e0697d14eb2a389615e621dc8fc48c354d4
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2022-01-09 23:33:03 +0100

    Add new activity and navgraph

--- app/src/main/java/com/joaobapt/todo/auth/AuthenticationFragment.kt
+++ app/src/main/java/com/joaobapt/todo/auth/AuthenticationFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.joaobapt.todo.R
 import com.joaobapt.todo.databinding.FragmentAuthenticationBinding
 
 class AuthenticationFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        // Inflate the layout for this fragment
+        
         val binding = FragmentAuthenticationBinding.inflate(inflater, container, false)
+        binding.authLogInButton.setOnClickListener {
+            findNavController().navigate(R.id.action_authenticationFragment_to_loginFragment)
+        }
+        binding.authSignUpButton.setOnClickListener {
+            findNavController().navigate(R.id.action_authenticationFragment_to_signupFragment)
+        }
         return binding.root
     }
 }
