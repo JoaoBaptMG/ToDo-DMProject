b797e1bbd6ba2d2d07d39c6d24094adcff8bc617
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2022-01-09 01:35:49 +0100

    Add view binding (TP1 done)

--- app/src/main/java/com/joaobapt/todo/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/joaobapt/todo/tasklist/TaskListAdapter.kt
@@ -1,29 +1,28 @@
 package com.joaobapt.todo.tasklist
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.joaobapt.todo.R
+import com.joaobapt.todo.databinding.ItemTaskBinding
 
 class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(TasksDiffCallback) {
-    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(taskTitle: String, taskDescription: String) {
-            itemView.findViewById<TextView>(R.id.task_title).text = taskTitle
-            itemView.findViewById<TextView>(R.id.task_description).text = taskDescription
-        }
-    }
-    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_task, parent, false)
-        return TaskViewHolder(itemView)
+        val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TaskViewHolder(binding)
     }
     
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val task = getItem(position)
         holder.bind(task.title, task.description)
+    }
+    
+    inner class TaskViewHolder(private val binding: ItemTaskBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        
+        fun bind(taskTitle: String, taskDescription: String) {
+            binding.taskTitle.text = taskTitle
+            binding.taskDescription.text = taskDescription
+        }
     }
 }
--- app/src/main/java/com/joaobapt/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/joaobapt/todo/tasklist/TaskListFragment.kt
@@ -9,30 +9,37 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.joaobapt.todo.R
+import com.joaobapt.todo.databinding.FragmentTaskListBinding
 import java.util.*
 
 class TaskListFragment : Fragment() {
+    private var _binding: FragmentTaskListBinding? = null
+    private val binding get() = _binding!!
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
-        return rootView
+                              savedInstanceState: Bundle?): View {
+        _binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        return binding.root
     }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val recyclerView = view.findViewById<RecyclerView>(R.id.task_list_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        
-        val adapter = TaskListAdapter()
-        recyclerView.adapter = adapter
-        adapter.submitList(taskList)
-        
-        val fab = view.findViewById<FloatingActionButton>(R.id.task_list_fab)
-        fab.setOnClickListener {
+        val taskListAdapter = TaskListAdapter()
+        binding.taskListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = taskListAdapter
+        }
+        taskListAdapter.submitList(taskList)
+    
+        binding.taskListFab.setOnClickListener {
             taskList = taskList + Task(id = UUID.randomUUID().toString(),
                                        title = "Task ${taskList.size + 1}")
-            adapter.submitList(taskList)
+            taskListAdapter.submitList(taskList)
         }
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
     
     private var taskList = listOf(
